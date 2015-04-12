01b9b8820ae8e5c1893497a3dcc96c06558813fe
xpollcon <xpollcon@192168015007.tpgi.com.au>
2015-04-12 11:16:04 +1000

    ver 1.0

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get -y install tomcat7
+apt-get -y install curl
+#apt-get -y install mongodb-10gen=2.2.3
+
+#mysql
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
+sudo apt-get update
+sudo apt-get -y install mysql-server-5.5
+
+#MongoDB
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+apt-get update
+apt-get -y install mongodb
